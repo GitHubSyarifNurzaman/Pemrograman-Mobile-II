e8d42f35c57bfd43e9333e6ea5920751bfdefe53
Syarif nurzaman <60865754+GitHubSyarifNurzaman@users.noreply.github.com>
2023-11-26 22:18:02 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Program Files\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\PROJECT CODING SYARIF NURZAMAN\\Flutter_semester5\\UTS_MOBILE_2\\uts_mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Program Files\\flutter"
+  "PROJECT_DIR=C:\\PROJECT CODING SYARIF NURZAMAN\\Flutter_semester5\\UTS_MOBILE_2\\uts_mobile"
+  "FLUTTER_ROOT=C:\\Program Files\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\PROJECT CODING SYARIF NURZAMAN\\Flutter_semester5\\UTS_MOBILE_2\\uts_mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\PROJECT CODING SYARIF NURZAMAN\\Flutter_semester5\\UTS_MOBILE_2\\uts_mobile"
+  "FLUTTER_TARGET=C:\\PROJECT CODING SYARIF NURZAMAN\\Flutter_semester5\\UTS_MOBILE_2\\uts_mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC83NGQxNjYyN2I5NDBiYjE1ZTUwODkxZjgyY2FkNmMzZTM0NjViZDZkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\PROJECT CODING SYARIF NURZAMAN\\Flutter_semester5\\UTS_MOBILE_2\\uts_mobile\\.dart_tool\\package_config.json"
+)
